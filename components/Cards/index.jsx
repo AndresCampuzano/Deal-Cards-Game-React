2349ca9e0d1806547652a0cc1f0d245821f6858f
Andres Campuzano <campuxano88@gmail.com>
2020-09-12 12:13:23 -0500

    Fixed: styles.

--- components/Cards/index.jsx
+++ components/Cards/index.jsx
@@ -1,36 +1,95 @@
 import React from 'react';
-import { Red, Black, Card } from './styles';
+import {
+    CardRed,
+    CardBlack,
+    ContentUp,
+    ContentDown,
+    Container
+} from './styles';
 
 const index = ({ value, icon }) => {
     return (
         <>
             {icon[0] === 'hearts' && (
-                <Card>
-                    <Red>
-                        {value[0]} {icon[0]}
-                    </Red>
-                </Card>
+                <CardRed>
+                    <Container>
+                        <ContentUp>
+                            {value[0]}
+                            <img
+                                src='https://storage.googleapis.com/deal_cards_game_next/heartImage%20(1).png'
+                                alt='hearts'
+                            />
+                        </ContentUp>
+                        <ContentDown>
+                            {value[0]}
+                            <img
+                                src='https://storage.googleapis.com/deal_cards_game_next/heartImage%20(1).png'
+                                alt='hearts'
+                            />
+                        </ContentDown>
+                    </Container>
+                </CardRed>
             )}
             {icon[0] === 'diamonds' && (
-                <Card>
-                    <Red>
-                        {value[0]} {icon[0]}
-                    </Red>
-                </Card>
+                <CardRed>
+                    <Container>
+                        <ContentUp>
+                            {value[0]}
+                            <img
+                                src='https://storage.googleapis.com/deal_cards_game_next/diamondImage%20(1).png'
+                                alt='hearts'
+                            />
+                        </ContentUp>
+                        <ContentDown>
+                            {value[0]}
+                            <img
+                                src='https://storage.googleapis.com/deal_cards_game_next/diamondImage%20(1).png'
+                                alt='hearts'
+                            />
+                        </ContentDown>
+                    </Container>
+                </CardRed>
             )}
             {icon[0] === 'spades' && (
-                <Card>
-                    <Black>
-                        {value[0]} {icon[0]}
-                    </Black>
-                </Card>
+                <CardBlack>
+                    <Container>
+                        <ContentUp>
+                            {value[0]}
+                            <img
+                                src='https://storage.googleapis.com/deal_cards_game_next/spadeImage.png'
+                                alt='hearts'
+                            />
+                        </ContentUp>
+                        <ContentDown>
+                            {value[0]}
+                            <img
+                                src='https://storage.googleapis.com/deal_cards_game_next/spadeImage.png'
+                                alt='hearts'
+                            />
+                        </ContentDown>
+                    </Container>
+                </CardBlack>
             )}
+
             {icon[0] === 'clubs' && (
-                <Card>
-                    <Black>
-                        {value[0]} {icon[0]}
-                    </Black>
-                </Card>
+                <CardBlack>
+                    <Container>
+                        <ContentUp>
+                            {value[0]}
+                            <img
+                                src='https://storage.googleapis.com/deal_cards_game_next/clubImage.png'
+                                alt='hearts'
+                            />
+                        </ContentUp>
+                        <ContentDown>
+                            {value[0]}
+                            <img
+                                src='https://storage.googleapis.com/deal_cards_game_next/clubImage.png'
+                                alt='hearts'
+                            />
+                        </ContentDown>
+                    </Container>
+                </CardBlack>
             )}
         </>
     );
--- components/Cards/styles.js
+++ components/Cards/styles.js
@@ -1,17 +1,49 @@
 import styled from 'styled-components';
 
-export const Red = styled.p`
-    color: ${({ theme }) => theme.red};
-`;
-
-export const Black = styled.p`
-    color: ${({ theme }) => theme.black};
-`;
-
-export const Card = styled.div`
+export const CardRed = styled.div`
     height: 190px;
     width: 130px;
     margin: 25px;
     border-radius: 5px;
-    border: 3px solid #b9b9b9;
+    border: 3px solid red;
+    background-color: white;
+    color: #ff0000;
+`;
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 100%;
+`;
+
+export const ContentUp = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin: 10px;
+    font-size: 22px;
+    img {
+        width: 22px;
+    }
+`;
+
+export const ContentDown = styled.div`
+    display: flex;
+    flex-direction: column-reverse;
+    transform: rotate(180deg);
+    margin: 10px;
+    font-size: 22px;
+    img {
+        width: 22px;
+    }
+`;
+
+export const CardBlack = styled.div`
+    height: 190px;
+    width: 130px;
+    margin: 25px;
+    border-radius: 5px;
+    border: 3px solid black;
+    background-color: white;
+    color: #000000;
 `;
