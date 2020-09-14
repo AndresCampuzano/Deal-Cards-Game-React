142df70cfe30476a36ca09f8bcaa3971f5a4f7e0
Andres Campuzano <campuxano88@gmail.com>
2020-09-14 07:11:08 -0500

    Fix: images loaded localy.

--- components/Cards/index.jsx
+++ components/Cards/index.jsx
@@ -1,98 +1,68 @@
-import React from 'react';
-import {
-    CardRed,
-    CardBlack,
-    ContentUp,
-    ContentDown,
-    Container
-} from './styles';
+import React from 'react'
+import { CardRed, CardBlack, ContentUp, ContentDown, Container } from './styles'
 
 const index = ({ value, icon }) => {
-    return (
-        <>
-            {icon[0] === 'hearts' && (
-                <CardRed>
-                    <Container>
-                        <ContentUp>
-                            {value[0]}
-                            <img
-                                src='https://storage.googleapis.com/deal_cards_game_next/heartImage%20(1).png'
-                                alt='hearts'
-                            />
-                        </ContentUp>
-                        <ContentDown>
-                            {value[0]}
-                            <img
-                                src='https://storage.googleapis.com/deal_cards_game_next/heartImage%20(1).png'
-                                alt='hearts'
-                            />
-                        </ContentDown>
-                    </Container>
-                </CardRed>
-            )}
-            {icon[0] === 'diamonds' && (
-                <CardRed>
-                    <Container>
-                        <ContentUp>
-                            {value[0]}
-                            <img
-                                src='https://storage.googleapis.com/deal_cards_game_next/diamondImage%20(1).png'
-                                alt='hearts'
-                            />
-                        </ContentUp>
-                        <ContentDown>
-                            {value[0]}
-                            <img
-                                src='https://storage.googleapis.com/deal_cards_game_next/diamondImage%20(1).png'
-                                alt='hearts'
-                            />
-                        </ContentDown>
-                    </Container>
-                </CardRed>
-            )}
-            {icon[0] === 'spades' && (
-                <CardBlack>
-                    <Container>
-                        <ContentUp>
-                            {value[0]}
-                            <img
-                                src='https://storage.googleapis.com/deal_cards_game_next/spadeImage.png'
-                                alt='hearts'
-                            />
-                        </ContentUp>
-                        <ContentDown>
-                            {value[0]}
-                            <img
-                                src='https://storage.googleapis.com/deal_cards_game_next/spadeImage.png'
-                                alt='hearts'
-                            />
-                        </ContentDown>
-                    </Container>
-                </CardBlack>
-            )}
+  return (
+    <>
+      {icon[0] === 'hearts' && (
+        <CardRed>
+          <Container>
+            <ContentUp>
+              {value[0]}
+              <img src='images/heartImage.png' alt='hearts' />
+            </ContentUp>
+            <ContentDown>
+              {value[0]}
+              <img src='images/heartImage.png' alt='hearts' />
+            </ContentDown>
+          </Container>
+        </CardRed>
+      )}
+      {icon[0] === 'diamonds' && (
+        <CardRed>
+          <Container>
+            <ContentUp>
+              {value[0]}
+              <img src='images/diamondImage.png' alt='diamonds' />
+            </ContentUp>
+            <ContentDown>
+              {value[0]}
+              <img src='images/diamondImage.png' alt='diamonds' />
+            </ContentDown>
+          </Container>
+        </CardRed>
+      )}
+      {icon[0] === 'spades' && (
+        <CardBlack>
+          <Container>
+            <ContentUp>
+              {value[0]}
+              <img src='images/spadeImage.png' alt='spades' />
+            </ContentUp>
+            <ContentDown>
+              {value[0]}
+              <img src='images/spadeImage.png' alt='spades' />
+            </ContentDown>
+          </Container>
+        </CardBlack>
+      )}
 
-            {icon[0] === 'clubs' && (
-                <CardBlack>
-                    <Container>
-                        <ContentUp>
-                            {value[0]}
-                            <img
-                                src='https://storage.googleapis.com/deal_cards_game_next/clubImage.png'
-                                alt='hearts'
-                            />
-                        </ContentUp>
-                        <ContentDown>
-                            {value[0]}
-                            <img
-                                src='https://storage.googleapis.com/deal_cards_game_next/clubImage.png'
-                                alt='hearts'
-                            />
-                        </ContentDown>
-                    </Container>
-                </CardBlack>
-            )}
-        </>
-    );
-};
+      {icon[0] === 'clubs' && (
+        <CardBlack>
+          <Container>
+            <ContentUp>
+              {value[0]}
+              <img src='images/clubImage.png' alt='clubs' />
+            </ContentUp>
+            <ContentDown>
+              {value[0]}
+              <img src='images/clubImage.png' alt='clubs' />
+            </ContentDown>
+          </Container>
+        </CardBlack>
+      )}
+    </>
+  )
+}
 
-export default index;
+export default index
